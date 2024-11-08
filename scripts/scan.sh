4cab12dd34809885b9a61ca80a557b945deb7c69
Anthony Fielding <ant@antfie.com>
2024-11-08 13:46:55 +0000

    Update scanning script

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -8,9 +8,9 @@ GREEN='\033[0;32m'
 CYAN='\033[1;36m'
 NC='\033[0m' # No Color
 
+
 mkdir -p scan
-rm -f -- scan/veracode-auto-pack-veracode_csv_mitigator-python.zip
-rm -f -- scan/*.json
+rm -rf -- scan/packages
 
 
 echo -e "\n${CYAN}Dependency check...${NC}"
@@ -26,12 +26,36 @@ cd ..
 
 
 echo -e "\n${CYAN}Packaging for SAST scanning...${NC}"
-./scan/veracode package --trust --source . --output scan --debug
+./scan/veracode package --trust --source . --output scan/packages
+zipFilePath="scan/packages/veracode-auto-pack-veracode_csv_mitigator-python.zip"
+entrypointModules="Python files within veracode-auto-pack-veracode_csv_mitigator-python.zip"
 
 
-echo -e "\n${CYAN}SAST Scanning with Veracode...${NC}"
-./scan/veracode static scan --baseline-file sast_baseline.json --results-file scan/sast_results.json scan/veracode-auto-pack-veracode_csv_mitigator-python.zip
+echo -e "\n${CYAN}SAST Scanning with Veracode (Pipeline)...${NC}"
+./scan/veracode static scan $zipFilePath \
+                            --baseline-file sast_baseline.json \
+                            --results-file scan/sast_results.json\
+                            --include "$entrypointModules"
 
+
+echo -e "\n${CYAN}SAST Scanning with Veracode (Policy)...${NC}"
+
+if [ ! -e "scan/veracode-api.jar" ]; then
+  # Refer to this page: https://central.sonatype.com/artifact/com.veracode.vosp.api.wrappers/vosp-api-wrappers-java/versions
+  apiWrapperVersion="24.10.15.0"
+  curl "https://repo1.maven.org/maven2/com/veracode/vosp/api/wrappers/vosp-api-wrappers-java/$apiWrapperVersion/vosp-api-wrappers-java-$apiWrapperVersion.jar" --output scan/veracode-api.jar
+fi
+
+java -jar scan/veracode-api.jar \
+     -action uploadandscan \
+     -appname "veracode_csv_mitigator" \
+     -createprofile false \
+     -version "`date "+%Y-%m-%d %H:%M:%S"`" \
+     -deleteincompletescan 2 \
+     -filepath scan/packages \
+     -include "$entrypointModules" \
+     -scanpollinginterval 30 \
+     -scantimeout 15
 
 echo -e "\n${CYAN}Generating SBOMs...${NC}"
 ./scan/veracode sbom --type archive --source scan/veracode-auto-pack-veracode_csv_mitigator-python.zip --output scan/src.sbom.json
